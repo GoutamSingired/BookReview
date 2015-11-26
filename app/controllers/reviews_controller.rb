db5a54efd39088e87d800f7e2c22712ed0c6d6e6
Goutam Singireddy <goutam.singireddy@gmail.com>
2015-11-25 23:21:03 -0500

    final commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :find_book
   before_action :find_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
  
 
 	def new
@@ -34,6 +35,13 @@ class ReviewsController < ApplicationController
 	  end
 
 	end	
+
+	def destroy
+      @review.destroy
+      redirect_to book_path(@book)
+
+	end
+		
 
 
   private
